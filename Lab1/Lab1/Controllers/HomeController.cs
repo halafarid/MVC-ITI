cbc20ca646fdf728258e8f968470b5bdc9358838
halafarid <hala.farid179@gmail.com>
2020-05-04 15:13:51 +0200

    Lab1

---
+++ Lab1/Lab1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(string name, string email, string phone, string attendance)
+        {
+            if (name != null && email != null)
+            {
+                ViewBag.Name = name;
+                return View("Welcome");
+            }
+            return View();
+        }
+    }
+}
